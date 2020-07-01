1752e38653dee4c51fd76081f56501ec1d57ae5d
1k-bhdn <1k.bhdn@gmail.com>
2020-07-01 10:01:07 +0300

    Продолжаем оформление декоративных элементов

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
 // Кнопки активации
-let writeUs = document.querySelector(".index-about__link--trigger");
-let mapLink = document.querySelector(".index-about__map-link");
+var writeUs = document.querySelector(".index-about__link--trigger");
+var mapLink = document.querySelector(".index-about__map-link");
+var buyButtons = document.querySelectorAll(".card__buy");
 
 // Форма обратной связи + содержимое
-let modalFeedback = document.querySelector(".modal--feedback");
-let feedbackForm = modalFeedback.querySelector(".modal__form");
-let userName = modalFeedback.querySelector("#feedback-name");
-let userEmail = modalFeedback.querySelector("#feedback-email");
-let userFeedback = modalFeedback.querySelector("#feedback-area");
-let feedbackClose = modalFeedback.querySelector(".modal__close");
+var modalFeedback = document.querySelector(".modal--feedback");
+if (modalFeedback) {
+  var feedbackForm = modalFeedback.querySelector(".modal__form");
+  var userName = modalFeedback.querySelector("#feedback-name");
+  var userEmail = modalFeedback.querySelector("#feedback-email");
+  var userFeedback = modalFeedback.querySelector("#feedback-area");
+  var feedbackClose = modalFeedback.querySelector(".modal__close");
+}
 
 // Интерактивная карта
-let modalMap = document.querySelector(".modal--map");
-let mapClose = modalMap.querySelector(".modal__close");
+var modalMap = document.querySelector(".modal--map");
+if (modalMap) {
+  var mapClose = modalMap.querySelector(".modal__close");
+}
 
 // Товар добавлен в корзину
+var modalNotification = document.querySelector(".modal--cart");
+var notificationClose = modalNotification.querySelector(".modal__close");
 
 // Локальное хранилище
-
-let isStorageSupport = true;
-let loginStorage = "";
-let emailStorage = "";
+var isStorageSupport = true;
+var loginStorage = "";
+var emailStorage = "";
 
 try {
   loginStorage = localStorage.getItem("login");
@@ -30,68 +36,97 @@ try {
 }
 
 // Показываем модальное окно
+for (var i = 0; i < buyButtons.length; i ++) {
+  var buyButton = buyButtons[i];
+  buyButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalNotification.classList.add("modal-show");
+  });
+}
 
-mapLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modalMap.classList.add("modal-show");
-});
+if (modalMap) {
+  mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalMap.classList.add("modal-show");
+  });
+}
 
-writeUs.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modalFeedback.classList.add("modal-show");
-  if (emailStorage || emailStorage) {
-    userName.value = loginStorage;
-    userEmail.value = emailStorage;
-    userFeedback.focus()
-  } else {
-    userName.focus()
-  }
-});
+if (modalFeedback) {
+  writeUs.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalFeedback.classList.add("modal-show");
+    if (emailStorage || emailStorage) {
+      userName.value = loginStorage;
+      userEmail.value = emailStorage;
+      userFeedback.focus()
+    } else {
+      userName.focus()
+    }
+  });
+}
 
 // Валидируем форму
-
-feedbackForm.addEventListener("submit", function (evt) {
-  if (!userName.value || !userEmail.value || !userFeedback.value) {
-    evt.preventDefault();
-    modalFeedback.classList.remove("modal-error");
-    modalFeedback.offsetWidth = modalFeedback.offsetWidth;
-    modalFeedback.classList.add("modal-error");
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("login", userName.value);
-      localStorage.setItem("email", userEmail.value);
+if (modalFeedback) {
+  feedbackForm.addEventListener("submit", function (evt) {
+    if (!userName.value || !userEmail.value || !userFeedback.value) {
+      evt.preventDefault();
+      modalFeedback.classList.remove("modal-error");
+      modalFeedback.offsetWidth = modalFeedback.offsetWidth;
+      modalFeedback.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", userName.value);
+        localStorage.setItem("email", userEmail.value);
+      }
     }
-  }
-});
+  });
+}
 
 // Закрываем модальное окно (кнопкой + esc)
-
-mapClose.addEventListener("click", function (evt) {
+notificationClose.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modalMap.classList.remove("modal-show");
+  modalNotification.classList.remove("modal-show");
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (modalMap.classList.contains("modal-show")) {
+    if (modalNotification.classList.contains("modal-show")) {
       evt.preventDefault();
-      modalMap.classList.remove("modal-show");
+      modalNotification.classList.remove("modal-show");
     }
   }
 });
 
-feedbackClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modalFeedback.classList.remove("modal-show");
-  modalFeedback.classList.remove("modal-error");
-});
+if (modalMap) {
+  mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalMap.classList.remove("modal-show");
+  });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (modalFeedback.classList.contains("modal-show")) {
-      evt.preventDefault();
-      modalFeedback.classList.remove("modal-show");
-      modalFeedback.classList.remove("modal-error");
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (modalMap.classList.contains("modal-show")) {
+        evt.preventDefault();
+        modalMap.classList.remove("modal-show");
+      }
     }
-  }
-});
+  });
+}
+
+if (modalFeedback) {
+  feedbackClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalFeedback.classList.remove("modal-show");
+    modalFeedback.classList.remove("modal-error");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (modalFeedback.classList.contains("modal-show")) {
+        evt.preventDefault();
+        modalFeedback.classList.remove("modal-show");
+        modalFeedback.classList.remove("modal-error");
+      }
+    }
+  });
+}
